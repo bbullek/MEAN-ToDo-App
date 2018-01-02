d76ba0347eb68fb6be145a9ec2a6b00dac49fb98
Brooke Bullek <bbullek@live.com>
2018-01-02 10:10:01 -0500

    Text entries dynamically show up in todo list while typing

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -2,14 +2,43 @@
 // Updated December 2017
 
 export default function($scope) {
-	$scope.todos = [
-	{
-		task: 'do dishes',
-		isCompleted: false
-	},
-	{
-		task: 'walk the dog',
-		isCompleted: true
-	}
-	]
+  let params = {
+    createHasInput: false
+  };
+
+  $scope.todos = [
+    {
+      task: 'do dishes',
+      isCompleted: false
+    },
+    {
+      task: 'walk the dog',
+      isCompleted: true
+    }
+  ];
+
+  $scope.onCompletedClick = function(todo) {
+    todo.isCompleted = !todo.isCompleted;
+  };
+
+  /*
+   * Monitor the textbox to append new tasks as they are added.
+   */
+  $scope.$watch('createTaskInput', function(val) {
+    // If the textbox was erased, remove the old task entry from todos
+    if (!val && params.createHasInput) {
+      $scope.todos.pop();
+      params.createHasInput = false;
+    }
+    // If the user types something for the first time, add a new task
+    if (val && !params.createHasInput) {
+      $scope.todos.push({ task: val, isCompleted: false });
+      params.createHasInput = true;
+    }
+    // If something was already entered into the textbox but the user 
+    // typed more, simply update the last entry
+    else if (val && params.createHasInput) {
+      $scope.todos[$scope.todos.length - 1].task = val;
+    }
+  });
 }
